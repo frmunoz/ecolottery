2da94373c6af14530bc380904efeda7da87438ab
Rekyt <matthias.grenie@ens-lyon.fr>
2016-01-22 18:20:54 +0100

    Briefs comments on metacommunity section

--- pkg/R/coalesc.R
+++ pkg/R/coalesc.R
@@ -3,26 +3,29 @@ coalesc <- function(J, theta, m = 1, filt = NULL, pool = NULL, Jpool = 50*J)
   #Create the regional pool if not provided
   if(is.null(pool)) 
   {
-    AA <- Jpool
-    A2 <- 1:Jpool
-    S2 <- array(0, c(AA, 1))
-    C2 <- array(0, c(AA, 1))
-    aa <- 1
-    ss <- 0
-    Y <- runif(AA)
+    AA <- Jpool # Total number of individuals in the pool
+    A2 <- 1:Jpool  # Labels of individuals
+    S2 <- array(0, c(AA, 1))  # Species labels
+    C2 <- array(0, c(AA, 1))  # Trait
+    aa <- 1  # ?
+    ss <- 0  # ?
+    Y <- runif(AA)  # Generate a vector to determine species
     
-    R2 <- theta / (theta + (1:AA) - 1)
-    mia <- which(Y <= R2)
+    R2 <- theta / (theta + (1:AA) - 1)  # Vector to determine species
+    mia <- which(Y <= R2)  # Get all individuals with different species
+    # All individuals that are reassigned to previous secies
     mib <- which(Y > R2)
     
-    S2[mia] <- 1:length(mia)
-    C2[mia] <- runif(length(mia))
+    S2[mia] <- 1:length(mia)  # Set species number
+    C2[mia] <- runif(length(mia))  # Compute Trait
     
+    # For all individuals without an assigned species
     for(j in 1:length(mib)) {
+      # Select randomly a previously assigned individual
       jj <- max(round(runif(1) * mib[j] - 1), 1)
-      A2[mib[j]] <- A2[jj]
-      S2[mib[j]] <- S2[jj]
-      C2[mib[j]] <- C2[jj]
+      A2[mib[j]] <- A2[jj]  # ??? Replace value in individual list
+      S2[mib[j]] <- S2[jj]  # Assign species of previously assigned individual
+      C2[mib[j]] <- C2[jj]  # Assign species trait
     }
     pool <- cbind(A2, S2, C2)
   }
