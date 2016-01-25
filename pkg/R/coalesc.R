86bb3833e2d7416396bbe02afe09fd3325fff4a2
Rekyt <matthias.grenie@ens-lyon.fr>
2016-01-25 11:32:45 +0100

    Modified code style to respect style guide

--- pkg/R/coalesc.R
+++ pkg/R/coalesc.R
@@ -1,8 +1,6 @@
-coalesc <- function(J, theta, m = 1, filt = NULL, pool = NULL, Jpool = 50*J)
-{
+coalesc <- function(J, theta, m = 1, filt = NULL, pool = NULL, Jpool = 50*J) {
   #Create the regional pool if not provided
-  if(is.null(pool)) 
-  {
+  if (is.null(pool)) {
     AA <- Jpool # Total number of individuals in the pool
     A2 <- 1:Jpool  # Labels of individuals
     S2 <- array(0, c(AA, 1))  # Species labels
@@ -20,7 +18,7 @@ coalesc <- function(J, theta, m = 1, filt = NULL, pool = NULL, Jpool = 50*J)
     C2[mia] <- runif(length(mia))  # Compute Trait
     
     # For all individuals without an assigned species
-    for(j in 1:length(mib)) {
+    for (j in 1:length(mib)) {
       # Select randomly a previously assigned individual
       jj <- max(round(runif(1) * mib[j] - 1), 1)
       A2[mib[j]] <- A2[jj]  # ??? Replace value in individual list
@@ -28,16 +26,20 @@ coalesc <- function(J, theta, m = 1, filt = NULL, pool = NULL, Jpool = 50*J)
       C2[mib[j]] <- C2[jj]  # Assign species trait
     }
     pool <- cbind(A2, S2, C2)
+  } else if (ncol(pool) < 2) {
+    stop("The regional pool is misdefined (at least two columns required)")
+  } else if (ncol(pool) == 2) {
+    warning("No trait information provided in the regional pool")
   }
   
-  else if(ncol(pool) < 2) {stop("The regional pool is misdefined (at least two columns required)")}
-  else if(ncol(pool) == 2) {warning("No trait information provided in the regional pool")}
-  
-  if(!is.null(filt)) {
+  if (!is.null(filt)) {
     filt1 <- function(x) filt(x)
-  } else {filt1 <- function(x) sapply(x, function(x) 1)}
+  } else {
+    filt1 <- function(x) sapply(x, function(x) 1)
+  }
   
-  if(!is.null(filt) & ncol(pool) == 2) {
+  
+  if (!is.null(filt) & ncol(pool) == 2) {
     pool[, 3] <- rep(1, nrow(pool))
   }
   
@@ -45,14 +47,15 @@ coalesc <- function(J, theta, m = 1, filt = NULL, pool = NULL, Jpool = 50*J)
   S <- array(0, c(J, 1))
   C <- array(0, c(J, 1))
   
-  if(m < 1)
-  {
+  if (m < 1) {
     I <- m *(J - 1) / (1 - m)
     X <- runif(J)
     R1 <- I / (I + (1:J) - 1)
     mi1 <- which(X <= R1)
     mi2 <- which(X > R1)
-  } else {mi1 = 1:J; mi2 = c()}
+  } else {
+    mi1 = 1:J; mi2 = c()
+  }
   
   AA <- length(mi1)
   
@@ -61,12 +64,18 @@ coalesc <- function(J, theta, m = 1, filt = NULL, pool = NULL, Jpool = 50*J)
   S[mi1] <- pool[migrants, 2]
   C[mi1] <- pool[migrants, 3]
   
-  for(j in 1:length(mi2)) {
-    if(j>1) {
-      if(A[mi2[j]]!= 0) {stop("Error in the assignation of ancestors")}
+  for (j in 1:length(mi2)) {
+    if (j > 1) {
+      
+      if (A[mi2[j]] != 0) {
+        stop("Error in the assignation of ancestors")
+      }
+      
       jj <- sample(c(mi1[mi1 < mi2[j]], mi2[1:(j - 1)]), 1)
     }
-    else {jj <- sample(mi1[mi1 < mi2[j]], 1)}
+    else {
+      jj <- sample(mi1[mi1 < mi2[j]], 1)
+    }
     
     A[mi2[j]] <- A[jj]
     S[mi2[j]] <- S[jj]
