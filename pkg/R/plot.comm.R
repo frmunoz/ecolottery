7a45ff0070caa91f261e9ca14022cae7258da445
Pierre DENELLE <pierre.denelle@cefe.cnrs.fr>
2016-09-20 13:46:19 +0200

    Plot comm function modified

--- pkg/R/plot.comm.R
+++ pkg/R/plot.comm.R
@@ -1,12 +1,21 @@
 # Plotting of regional and local trait distributions
 
-plot.comm <- function(x,seltrait=1) 
+plot.comm <- function(x, seltrait=1) 
 # x should be the output of coalesc or forward
 # seltrait is the index of the trait to be plotted (in case of multiple traits)
 {
   require(ggplot2)
+  # Color vectors
   metaCol <- rgb(1,0,0,0.2); localCol <- rgb(0,0,1,0.2)
-  data <- data.frame(level=c(rep("pool",nrow(x$pool)),rep("comm",nrow(x$com))),trait=c(x$pool[,seltrait+2],x$com[,seltrait+2]))
-  qplot(trait, data = data, geom = "density" , group = level, fill = level, alpha=.3)
+  
+  # Dataframe of trait pool and local values
+  data <- data.frame(level=c(rep("pool", nrow(x$pool)),rep("comm", nrow(x$com))),
+                     trait=c(x$pool[, seltrait+2],x$com[, seltrait+2]))
+  
+  # Plot
+  ggplot(data, aes(trait)) +
+    geom_density(aes(group = level, fill = level), alpha = 0.5) +
+    scale_fill_manual(values = c(metaCol, localCol)) +
+    theme_classic()
 }
  
