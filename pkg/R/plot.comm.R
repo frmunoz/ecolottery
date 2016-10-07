b76533b5c35b1280bbc3857ce4fc04e6c07eddf6
frmunoz <francois.munoz@cirad.fr>
2016-10-07 15:24:31 +0530

    New option abund

--- pkg/R/plot.comm.R
+++ pkg/R/plot.comm.R
@@ -1,22 +1,29 @@
-# Plotting of regional and local trait distributions
+# Plotting of regional and local trait or abundance distributions
 
-plot.comm <- function(x, seltrait=1,main=NULL) 
+plot.comm <- function(x, type="trait", seltrait=1,main=NULL) 
 # x should be the output of coalesc or forward
 # seltrait is the index of the trait to be plotted (in case of multiple traits)
 {
-  require(ggplot2)
-  # Color vectors
-  metaCol <- rgb(1,0,0,0.2); localCol <- rgb(0,0,1,0.2)
-  
-  # Dataframe of trait pool and local values
-  data <- data.frame(level=c(rep("pool", nrow(x$pool)),rep("comm", nrow(x$com))),
+  switch(type,
+    "trait"=
+     {
+       require(ggplot2)
+       # Color vectors
+       metaCol <- rgb(1,0,0,0.2); localCol <- rgb(0,0,1,0.2)
+       # Dataframe of trait pool and local values
+       data <- data.frame(level=c(rep("pool", nrow(x$pool)),rep("comm", nrow(x$com))),
                      trait=c(x$pool[, seltrait+2],x$com[, seltrait+2]))
-  
-  # Plot
-  ggplot(data, aes(trait),main=main) +
-    geom_density(aes(group = level, fill = level), alpha = 0.5) +
-    scale_fill_manual(values = c(metaCol, localCol)) +
-    ggtitle(main) + 
-    theme_classic()
+       # Plot
+       ggplot(data, aes(trait),main=main) + geom_density(aes(group = level, fill = level), alpha = 0.5) +
+        scale_fill_manual(values = c(metaCol, localCol)) +
+        ggtitle(main) + 
+        theme_classic()
+     },
+    "abund"=
+     {
+      ab <- abund(x) 
+      plot(ab$reg[names(ab$loc)]/sum(ab$reg),ab$loc/sum(ab$loc),main=main,xlab="Regional abundance", ylab="Local abundance",log="xy"); abline(0,1)
+     },
+    warning("Need to choose a type of plot to display (trait or abund)"))
 }
  
