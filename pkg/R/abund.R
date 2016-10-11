0468e6a34f338650736006dcacfb82d28969ab90
Rekyt <matthias.grenie@hotmail.com>
2016-10-11 18:35:19 +0200

    Improved readability and consistency #6
    Added comments in 'abund()' to explain the functioning. Improved
    consistency by using the same names for variables as in 'coalesc()' or
    'forward()' which are 'pool' and 'com' instead of 'loc' and 'reg'.

--- pkg/R/abund.R
+++ pkg/R/abund.R
@@ -1,11 +1,21 @@
+# From an object output by 'forward()' or 'coalesc()' computes vector of
+# abundances per species
 abund <- function(x)
 {
+  # Abundances of species in local community
   loc_t <- as.data.frame(table(x$com[,2]))
-  loc <- loc_t[,2]; names(loc) <- loc_t[,1]
-  if(!is.null(x$pool))
+  loc <- loc_t[,2]  # Get a flat vector of abundances
+  names(loc) <- loc_t[,1]
+  
+  if (!is.null(x$pool))
   {
+    # If the is a local pool computes regional level abundances
     reg_t <- as.data.frame(table(x$pool[,2]))
-    reg <- reg_t[,2]; names(reg) <- reg_t[,1]
-    return(list(loc=loc, reg=reg))
-  } else return(list(loc=loc))
+    reg <- reg_t[,2]
+    names(reg) <- reg_t[,1]
+    return(list(com = loc, pool = reg))
+  } else {
+    # If there is only a local community
+    return(list(com = loc))
+  }
 }
