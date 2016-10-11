8f715c8f385d22360a8177bb010ecc8648b8b479
Rekyt <matthias.grenie@hotmail.com>
2016-10-11 18:00:10 +0200

    Comments and explicit variables in 'tcor()' #6
    Added comments and explicit variable in function 'tcor()'. Also completed
    the manual file because, added for example undocumented arguments and
    formatted the manual file better.

--- pkg/R/tcor.R
+++ pkg/R/tcor.R
@@ -1,35 +1,47 @@
-# Returns a data frame of two variables which correlate with a population correlation of rho
-# If desired, one of both variables can be fixed to an existing variable by specifying x
+# Generates a data.frame with two correlated traits with the given 'mar.fun'
+# function correlated by a factor 'rho'. Or generates a trait vector correlated
+# to the given one 'x' by a factor 'rho'. 'n' gives the number of values
 
 tcor <- function(n, rho = 0.5, mar.fun = rnorm, x = NULL, ...) {
-  if (!is.null(x)) {X1 <- x} else {X1 <- mar.fun(n, ...)}
-  if (!is.null(x) & length(x) != n) warning("Variable x does not have the same length as n!")
   
-  if (rho > 1 | rho < 0) stop("rho must belong to [0; 1] value interval")
   
-  C <- matrix(rho, nrow = 2, ncol = 2)
-  diag(C) <- 1
-  
-  C <- chol(C)
-  
-  X2 <- mar.fun(n)
-  X <- cbind(X1, X2)
-  
-  # induce correlation (does not change X1)
-  df <- X %*% C
-  
-  ## if desired: check results
-  #all.equal(X1, X[,1])
-  #cor(X)
-  
-  # Formatting result into a dataframe
-  df <- as.data.frame(df)
-  # Naming columns
-  if (is.null(x)){
-    colnames(df) <- c("t1", "t2")
-  } else{
-    colnames(df) <- c("trait", "t2")
+  if (!is.null(x)) {
+    # If the user provides a target trait vector uses it
+    first_trait <- x
+  } else {
+    # Otherwise generates the first trait vector with provided function
+    first_trait <- mar.fun(n, ...)
   }
   
-  return(df)
+  # If the provided vector by the user is different from the target vector size
+  if (!is.null(x) & length(x) != n) {
+    warning("Trait vector x does not have length n!")
+  }
+  
+  # correlation needs to be strictly between 0 and 1 
+  if (rho > 1 | rho < 0) stop("rho must belong to ]0; 1[ value interval")
+  
+  
+  # Generate a correlation matrix with given correlation coefficient
+  corr_mat <- matrix(rho, nrow = 2, ncol = 2)
+  diag(corr_mat) <- 1
+  
+  corr_mat <- chol(corr_mat)
+  
+  second_trait <- mar.fun(n)
+  trait_mat <- cbind(first_trait, second_trait)
+  
+  # Induces correlation (does not change first_trait)
+  trait_df <- trait_mat %*% corr_mat
+  
+  # Formatting result into a dataframe
+  trait_df <- as.data.frame(trait_df)
+  # Naming columns
+  if (is.null(x)) {
+    colnames(trait_df) <- c("t1", "t2")
+  } else{
+    colnames(trait_df) <- c("trait", "t2")
+  }
+  
+  return(trait_df)
 }
