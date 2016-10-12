2730d412ff3902903b0da85e01a6aad00b6f0d2e
Rekyt <matthias.grenie@hotmail.com>
2016-10-12 15:10:52 +0200

    Comments in pick, pick.mutate and pick.immigrate

--- pkg/R/forward.R
+++ pkg/R/forward.R
@@ -46,7 +46,7 @@ forward <- function(initial, prob = 0, D = 1, gens = 150, keep = FALSE,
   
   # If species pool is specified by user
   if (!is.null(pool)) {
-    if (ncol(pool)<2) {
+    if (ncol(pool) < 2) {
       stop(paste0("The regional pool is misdefined (at least two columns ",
                   "required when a matrix or data frame is provided)"))
     } else if (ncol(pool) == 2) {
@@ -151,7 +151,7 @@ forward <- function(initial, prob = 0, D = 1, gens = 150, keep = FALSE,
                 limit.sim.t = limit.sim.t,
                 pool = pool))
   
-  } else {  # Keep only the last community
+  } else {# Keep only the last community
     for (i in 1:gens) {
       # Simulate community dynamics for a timestep
       next_comm <- pick(next_comm, D = D, prob = prob, pool = pool,
@@ -160,148 +160,229 @@ forward <- function(initial, prob = 0, D = 1, gens = 150, keep = FALSE,
                         filt = filt, new.index = new.index)
       
       new.index <- next_comm$new.index
-      next_comm <- next_comm$a
+      next_comm <- next_comm$com
     }
     return(list(com = next_comm, pool = pool))
   }
 }
-  
-# Return "mutate" individuals if pool = NULL, else return immigrate individuals
-pick <- function(a, D = 1, prob = 0, pool = NULL, prob.death = prob.death,
+
+# Precise function to simulate a single timestep by picking an individual in
+# the pool or make an individual mutate
+pick <- function(com, D = 1, prob = 0, pool = NULL, prob.death = prob.death,
                  limit.sim = NULL, coeff.lim.sim = 2, sigma = 0.1, filt = NULL,
                  new.index = new.index) {
-  if(is.null(pool)) {
-    return(pick.mutate(a, D = D, prob.of.mutate = prob, new.index = new.index)) 
+  
+  
+  if (is.null(pool)) {
+    # If no species pool specified, mutate an individual
+    return(pick.mutate(com, D = D, prob.of.mutate = prob, new.index = new.index)) 
+  
   } else {
-	return(pick.immigrate(a, D = D, prob.of.immigrate = prob, pool = pool,
-	                      prob.death = prob.death, limit.sim = limit.sim,
-	                      coeff.lim.sim = coeff.lim.sim, sigma = sigma,
-	                      filt = filt))
+	  
+    # If there is a species pool make an individual immigrates
+    return(pick.immigrate(com, D = D, prob.of.immigrate = prob, pool = pool,
+                          prob.death = prob.death, limit.sim = limit.sim,
+                          coeff.lim.sim = coeff.lim.sim, sigma = sigma,
+                          filt = filt))
   }
 }
 
-# Return "mutate" individuals
-pick.mutate <- function(a, D = 1, prob.of.mutate = 0, new.index = 0) {
-  if (is.vector(a)) {
-    J <- length(a)
-    a <- data.frame(id = paste("ind", 1:J, sep = ""), sp = as.character(a), trait = rep(NA, J), stringsAsFactors = F)
-  }
-  else if (is.matrix(a) | is.data.frame(a)) {
-    J <- nrow(a)
-    if (is.matrix(a)) {
-      a <- as.data.frame(a, stringsAsFactors = F)
-      a[, 1] <- as.character(a[, 2])
+# Return community with mutated inidividual (= new species)
+pick.mutate <- function(com, D = 1, prob.of.mutate = 0, new.index = 0) {
+  
+  if (is.vector(com)) {
+    # If community only defined by species names
+    J <- length(com)
+    
+    com <- data.frame(id = paste("ind", 1:J, sep = ""),
+                      sp = as.character(com),
+                      trait = rep(NA, J),
+                      stringsAsFactors = F)
+  
+  } else if (is.matrix(com) | is.data.frame(com)) {
+    # If the community has defined traits
+    J <- nrow(com)
+    
+    if (is.matrix(com)) {
+      com <- as.data.frame(com, stringsAsFactors = F)
+      com[, 1] <- as.character(com[, 2])
     }
+    
   } else {
     stop("pick.mutate: misdefined community composition")
   }
   
+  ## Simulate the dynamics of the community
+  
+  # Number of individuals who die at this timestep
   died <- sample(J, D, replace = TRUE)
+  
+  # How many of the dead individuals are replaced by mutated individuals
   mutated <- runif(length(died)) < prob.of.mutate
-  J1 <- sum(mutated)
-  J2 <- sum(!mutated)
-  a[died[!mutated], ] <- a[sample(1:nrow(a), J2, replace = TRUE), ]
-  if (J1 > 0) {
-    # When a mutation occurs, the same individual id and species name are provided
-    a[died[mutated], 1:2] <- paste("new.sp", new.index + (1:J1), sep = "")
-    a[died[mutated], 3] <- rep(NA, J1)
-    new.index <- new.index + J1
+  
+  # Number of mutated individuals
+  n_mutated <- sum(mutated)
+  
+  # Dead individuals who did not mutate
+  dead_non_mutated <- sum(!mutated)
+  
+  # Replace dead non mutated individuals by individuals from other species
+  com[died[!mutated], ] <- com[sample(1:nrow(com), dead_non_mutated,
+                                      replace = TRUE), ]
+  
+  # When mutation occurs
+  if (n_mutated > 0) {
+    
+    # Attribute new species to individuals who mutate
+    com[died[mutated], 1:2] <- paste("new.sp", new.index + (1:n_mutated),
+                                     sep = "")
+    
+    com[died[mutated], 3] <- rep(NA, n_mutated)  # No trait values
+    
+    # Number of new species which appeared (next one will be new.index + 1)
+    new.index <- new.index + n_mutated
   }
-  return(list(com = a, new.index = new.index))
+  
+  return(list(com = com, new.index = new.index))
 }
 
-# Return "immigrate" individuals
-# limit.sim = distances de traits; filt = fonction pour habitat filtering
-pick.immigrate <- function(a, D = 1, prob.of.immigrate = 0, pool, prob.death = NULL, limit.sim = NULL, coeff.lim.sim = 2, sigma = 0.1, filt = NULL) {
-  if (is.vector(a)) {
-    J <- length(a)
-    a <- data.frame(id = paste("ind", 1:J, sep = ""), sp = as.character(a), trait = rep(NA, J), stringsAsFactors = F)
-  }
-  else if (is.matrix(a) | is.data.frame(a)) {
-    J <- nrow(a)
-    if (is.matrix(a)) {
-      a <- as.data.frame(a, stringsAsFactors = F)
-      a[, 2] <- as.character(a[, 2])
+# Function to return individuals who immigrated from the species pool
+# limit.sim = distances de traits; filt = habitat filtering function
+pick.immigrate <- function(com, D = 1, prob.of.immigrate = 0, pool,
+                           prob.death = NULL, limit.sim = NULL,
+                           coeff.lim.sim = 2, sigma = 0.1, filt = NULL) {
+  
+  if (is.vector(com)) {
+    # If community only defined by species names
+    J <- length(com)
+    
+    com <- data.frame(id = paste("ind", 1:J, sep = ""),
+                    sp = as.character(com),
+                    trait = rep(NA, J),
+                    stringsAsFactors = F)
+  
+  } else if (is.matrix(com) | is.data.frame(com)) {
+    # If the community has defined traits
+    J <- nrow(com)
+    
+    if (is.matrix(com)) {
+      com <- as.data.frame(com, stringsAsFactors = F)
+      com[, 2] <- as.character(com[, 2])
     }
-  } 
-  else stop("pick.immigrate: misdefined community composition")
+  
+  } else {
+    stop("pick.immigrate: misdefined community composition")
+  }
   
   # Function defining habitat filtering according to trait value
   if (!is.null(filt)) {
-    filt1 <- function(x) filt(x)
+    hab_filter <- function(x) filt(x)
   } else {
-	  filt1 <- function(x) sapply(x, function(x) 1)
+    # If no function defined, dummy function returning one
+	  hab_filter <- function(x) sapply(x, function(x) 1)
   }
   
-  a.init <- a
-  # For debug:
-  #print(a[(nrow(a) - 10):nrow(a), ])
-  #print(head(pool))
+  # Initial community
+  com.init <- com
+  
   if (is.null(limit.sim) & is.null(filt)) {
+    
     died <- sample(J, D, replace = TRUE)
-    a <- a[ - died, ]
-    if (any(is.na(a[, 1]))) {
+    
+    com <- com[-died, ]
+    
+    if (any(is.na(com[, 1]))) {
       stop("Error: NA values in community composition (1)")
       }
+  
   } else {# Influence of limiting similarity and habitat filtering on mortality
+    
     # Vector of the individual probability of dying
     if (is.null(prob.death)) {
-      prob.death <- rep(1, nrow(a))
+      prob.death <- rep(1, nrow(com))
     }
-    # Under limiting similarity, mortality increases when an individual is more similar to other resident individuals
+    
+    # Under limiting similarity, mortality increases when an individual is more
+    # similar to other resident individuals
     if (!is.null(limit.sim)) {
-      if (sum(!a[, 1] %in% rownames(limit.sim)) > 0 | sum(!a[, 1] %in% colnames(limit.sim)) > 0) {
+      
+      if (sum(!com[, 1] %in% rownames(limit.sim)) > 0 |
+          sum(!com[, 1] %in% colnames(limit.sim)) > 0) {
         stop("limit.sim: mismatch of species names")
       }
-      limit.sim.t <- apply(limit.sim[a[, 1], a[, 1]], 2, function(x) (sum(exp( - x^2 / (2*sigma^2)))))
+      
+      limit.sim.t <- apply(limit.sim[com[, 1], com[, 1]], 2,
+                           function(x) (sum(exp( -x^2 / (2*(sigma^2))))))
+      
       prob.death <- (coeff.lim.sim - 1)*limit.sim.t
+      
       limit.sim.t <- mean(limit.sim.t)
     }
     # Habitat filtering also influences the individual death probability
     if (!is.null(filt)) {
-      if (any(is.na(filt1(a[, 3])))) {
-        stop("Error: NA values in habitat filtering")
+      if (any(is.na(hab_filter(com[, 3])))) {
+        stop("Error: NA values in habitat filter")
       }
-      prob.death <- prob.death * (1 - filt1(a[, 3]) / sum(filt1(a[, 3])))
+      prob.death <- prob.death * (1 - hab_filter(com[, 3]) / sum(hab_filter(com[, 3])))
     }
+    
     died <- rmultinom(1, D, prob.death)
-    a <- a[ -died, ] # Community composition after mortality
-    if (sum(is.na(a[, 1])) != 0) {
+    
+    com <- com[ -died, ] # Community composition after mortality
+    
+    if (sum(is.na(com[, 1])) != 0) {
       stop("Error: NA values in community composition (2)")
     }
   } 
+  
   immigrated <- runif(D) < prob.of.immigrate
+  
   J1 <- sum(immigrated)
   J2 <- sum(!immigrated)
+  
   if (J1 > 0) {
     # Influence of habitat filtering on immigration
     if (is.null(filt)) {
       add <- pool[sample(1:nrow(pool), J1, replace = TRUE), 1:3]
-      a <- rbind(a, add)
-      if (sum(is.na(a[, 1])) != 0) {
+      com <- rbind(com, add)
+      
+      if (sum(is.na(com[, 1])) != 0) {
         stop("Error: NA values in immigrants")
       }
     } else {
-      a <- rbind(a, pool[sample(1:nrow(pool), J1, replace = TRUE, prob = filt1(pool[, 3])), 1:3])
-      if (any(is.na(filt1(pool[, 3])))) {
+      com <- rbind(com, pool[sample(1:nrow(pool), J1, replace = TRUE,
+                                prob = hab_filter(pool[, 3])), 1:3])
+      
+      if (any(is.na(hab_filter(pool[, 3])))) {
         stop("Error: NA values in habitat filtering")
       }
     }
-    if (any(is.na(a[, 1]))) {
+    
+    if (any(is.na(com[, 1]))) {
       stop("Error: NA values in community composition (3)")
       }
   }
+  
   if (J2 > 0) {
-    a <- rbind(a, a.init[sample(1:nrow(a.init), J2, replace = TRUE), 1:3])
-    if (any(is.na(a[, 1]))) {
+    
+    com <- rbind(com, com.init[sample(1:nrow(com.init), J2, replace = TRUE), 1:3])
+    
+    if (any(is.na(com[, 1]))) {
       print(J2)
       stop("Error: NA values in community composition (4)")
+    
     }
   }
+  
+  
   if (!is.null(limit.sim)) {
-    return(list(a = a, limit.sim.t = limit.sim.t))
+    # If there limiting similarity return the factor
+    return(list(com = com, limit.sim.t = limit.sim.t))
+  
   } else {
-    return(list(a = a))
+    # Without limiting similarity
+    return(list(com = com))
+  
   }
 }
 
