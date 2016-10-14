8a9315bb2acca4ba212e3732009046cb82dd2755
Pierre DENELLE <pierre.denelle@cefe.cnrs.fr>
2016-10-14 19:14:00 +0200

    abund function modified: data frames as outputs and relative abundance added

--- pkg/R/abund.R
+++ pkg/R/abund.R
@@ -1,11 +1,22 @@
 abund <- function(x)
 {
-  loc_t <- as.data.frame(table(x$com[,2]))
-  loc <- loc_t[,2]; names(loc) <- loc_t[,1]
+  if(!is.null(x$com)){
+    loc <- as.data.frame(table(x$com[, "sp"]))
+    colnames(loc) <- c("sp", "ab")
+    loc$abrel <- loc$ab / length(x$com$ind)
+  }
+  else{
+    stop("No local community provided, generate it with coalesc function")
+  }
+  
   if(!is.null(x$pool))
   {
-    reg_t <- as.data.frame(table(x$pool[,2]))
-    reg <- reg_t[,2]; names(reg) <- reg_t[,1]
-    return(list(loc=loc, reg=reg))
-  } else return(list(loc=loc))
+    reg <- as.data.frame(table(x$pool[, "sp"]))
+    colnames(reg) <- c("sp", "ab")
+    reg$abrel <- reg$ab / length(x$pool$ind)
+    return(list(loc = loc, reg = reg))
+  } else{
+    warning("No regional pool provided")
+    return(list(loc = loc))
+  }
 }
