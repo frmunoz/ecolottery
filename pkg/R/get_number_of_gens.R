131a0699bb0b06a1f0d3011f7a30c04bd12fca2c
Rekyt <matthias.grenie@hotmail.com>
2017-03-06 15:03:30 +0100

    Changepoint dependency

--- pkg/R/get_number_of_gens.R
+++ pkg/R/get_number_of_gens.R
@@ -3,44 +3,47 @@ get_number_of_gens <- function(given_size, pool, nbrep = 100, prob = 0, d = 1,
                                sigm = 0.1, filt = NULL, prob.death = NULL,
                                method.dist = "euclidean", plot_gens = FALSE) {
   
-  # Simulate starting community from given pool
-  start_com <- pool[sample(1:nrow(pool), given_size),]
-  
-  # Gives the number of generation to test
-  if (is.null(gens)) {
-    gens <- 100*given_size
-  }
+  if (requireNamespace("changepoint", quietly = TRUE)) {
     
-  if (is.null(filt)) {
-    filt <- function(x) return(1)
-  }
-  
-  gens_conv_cpt <- c()
-  nb_sp_gen <- c()
-  
-  # Loop to simulate the communities and determine changepoint
-  for (i in 1:nbrep) {
-    final <- forward(initial = start_com, prob = prob, d = d, gens = gens,
-                     keep = F, pool = pool, limit.sim = limit.sim, 
-                     coeff.lim.sim = coeff.lim.sim, sigm = sigm, filt = filt,
-                     prob.death = prob.death, method.dist = method.dist,
-                     plot_gens = plot_gens)
+    # Simulate starting community from given pool
+    start_com <- pool[sample(1:nrow(pool), given_size),]
     
+    # Gives the number of generation to test
+    if (is.null(gens)) {
+      gens <- 100*given_size
+    }
     
-    nb_sp <- final$sp_t
-    nb_sp_gen <- rbind(nb_sp_gen, data.frame(gens = 1:gens, rich = nb_sp,
-                                             stringsAsFactors = F))
+    if (is.null(filt)) {
+      filt <- function(x) return(1)
+    }
     
-    gens_conv_cpt <- c(gens_conv_cpt, cpts(cpt.mean(nb_sp, penalty = "BIC")))
-  }
-  
-  # Average number of generations
-  nb_sp_gen <- data.frame(apply(nb_sp_gen, 2,
-                                function(x) tapply(x, nb_sp_gen$gens, mean)))
-  
-  if (requireNamespace("ggplot2", quietly = TRUE)) {
+    gens_conv_cpt <- c()
+    nb_sp_gen <- c()
+    
+    # Loop to simulate the communities and determine changepoint
+    for (i in 1:nbrep) {
+      final <- forward(initial = start_com, prob = prob, d = d, gens = gens,
+                       keep = F, pool = pool, limit.sim = limit.sim, 
+                       coeff.lim.sim = coeff.lim.sim, sigm = sigm, filt = filt,
+                       prob.death = prob.death, method.dist = method.dist,
+                       plot_gens = plot_gens)
+      
+      
+      nb_sp <- final$sp_t
+      nb_sp_gen <- rbind(nb_sp_gen, data.frame(gens = 1:gens, rich = nb_sp,
+                                               stringsAsFactors = F))
+      
+      cpt_bic <- changepoint::cpt.mean(nb_sp, penalty = "BIC")
+      
+      gens_conv_cpt <- c(gens_conv_cpt, changepoint::cpts(cpt_bic))
+    }
+    
+    # Average number of generations
+    nb_sp_gen <- data.frame(apply(nb_sp_gen, 2,
+                                  function(x) tapply(x, nb_sp_gen$gens, mean)))
+    
     # Make a plot to check if it is really convergent
-    diagnostic_plot <- ggplot(nb_sp_gen, aes(gens, rich)) +
+    diagnostic_plot <- ggplot(nb_sp_gen, aes_string("gens", "rich")) +
       geom_point(alpha = 0.5) +
       geom_vline(xintercept = max(gens_conv_cpt), colour = "red",
                  linetype = "dashed") +
@@ -48,10 +51,11 @@ get_number_of_gens <- function(given_size, pool, nbrep = 100, prob = 0, d = 1,
       theme(axis.text = element_text(size = 12),
             axis.title = element_text(size = 14, face = "bold")) +
       theme_bw()
+    
+    return(list(n = max(gens_conv_cpt), plot = diagnostic_plot))
+  
   } else {
-    message("ggplot2 is needed to have a diagnostic plot")
-    diagnostic_plot <- NULL
+    stop("Install package 'changepoint' to compute number of generations")
   }
   
-  return(list(n = max(gens_conv_cpt), plot = diagnostic_plot))
 }
