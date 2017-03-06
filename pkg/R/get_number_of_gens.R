24ceb5b00a19511b9fe8b342b4ba96b4da99ee79
Rekyt <matthias.grenie@hotmail.com>
2017-03-06 14:35:08 +0100

    Better indentation and comments in forward()

--- pkg/R/get_number_of_gens.R
+++ pkg/R/get_number_of_gens.R
@@ -1,24 +1,31 @@
-get_number_of_gens = function(given_size, pool, nbrep = 100, prob = 0, d = 1, gens = NULL, limit.sim = F,
-                              coeff.lim.sim = 1, sigm = 0.1, filt = NULL, prob.death = NULL,
-                              method.dist = "euclidean", plot_gens = FALSE)
-{
-  start_com = pool[sample(1:nrow(given_pool), given_size),]
+get_number_of_gens <- function(given_size, pool, nbrep = 100, prob = 0, d = 1,
+                               gens = NULL, limit.sim = F, coeff.lim.sim = 1,
+                               sigm = 0.1, filt = NULL, prob.death = NULL,
+                               method.dist = "euclidean", plot_gens = FALSE) {
   
-  if(is.null(gens))
-    gens = 100*given_size
+  # Simulate starting community from given pool
+  start_com <- pool[sample(1:nrow(pool), given_size),]
   
-  if(is.null(filt))
+  # Gives the number of generation to test
+  if (is.null(gens)) {
+    gens <- 100*given_size
+  }
+    
+  if (is.null(filt)) {
     filt <- function(x) return(1)
+  }
   
   gens_conv_cpt <- c()
   nb_sp_gen <- c()
   
-  for(i in 1:nbrep)
-  {
-    final <- forward(initial = start_com,
-                     prob = prob, d = d, gens = gens, keep = F, pool = pool, limit.sim = limit.sim, 
-                     coeff.lim.sim = coeff.lim.sim, sigm = sigm, filt = filt, prob.death = prob.death, 
-                     method.dist = method.dist, plot_gens = plot_gens)
+  # Loop to simulate the communities and determine changepoint
+  for (i in 1:nbrep) {
+    final <- forward(initial = start_com, prob = prob, d = d, gens = gens,
+                     keep = F, pool = pool, limit.sim = limit.sim, 
+                     coeff.lim.sim = coeff.lim.sim, sigm = sigm, filt = filt,
+                     prob.death = prob.death, method.dist = method.dist,
+                     plot_gens = plot_gens)
+    
     
     nb_sp <- final$sp_t
     nb_sp_gen <- rbind(nb_sp_gen, data.frame(gens = 1:gens, rich = nb_sp,
@@ -27,17 +34,24 @@ get_number_of_gens = function(given_size, pool, nbrep = 100, prob = 0, d = 1, ge
     gens_conv_cpt <- c(gens_conv_cpt, cpts(cpt.mean(nb_sp, penalty = "BIC")))
   }
   
-  nb_sp_gen <- data.frame(apply(nb_sp_gen, 2, function(x) tapply(x, nb_sp_gen$gens, mean)))
+  # Average number of generations
+  nb_sp_gen <- data.frame(apply(nb_sp_gen, 2,
+                                function(x) tapply(x, nb_sp_gen$gens, mean)))
   
-  # Make a plot to check if it is really convergent
-  diagnostic_plot = ggplot(nb_sp_gen, aes(gens, rich)) +
-    geom_point(alpha = 0.5) +
-    geom_vline(xintercept = max(gens_conv_cpt), colour = "red",
-               linetype = "dashed") +
-    labs(x = "Generation number", y = "Specific richness") +
-    theme(axis.text = element_text(size = 12),
-          axis.title = element_text(size = 14, face = "bold")) +
-    theme_bw()
+  if (requireNamespace("ggplot2", quietly = TRUE)) {
+    # Make a plot to check if it is really convergent
+    diagnostic_plot <- ggplot(nb_sp_gen, aes(gens, rich)) +
+      geom_point(alpha = 0.5) +
+      geom_vline(xintercept = max(gens_conv_cpt), colour = "red",
+                 linetype = "dashed") +
+      labs(x = "Generation number", y = "Specific richness") +
+      theme(axis.text = element_text(size = 12),
+            axis.title = element_text(size = 14, face = "bold")) +
+      theme_bw()
+  } else {
+    message("ggplot2 is needed to have a diagnostic plot")
+    diagnostic_plot <- NULL
+  }
   
   return(list(n = max(gens_conv_cpt), plot = diagnostic_plot))
 }
