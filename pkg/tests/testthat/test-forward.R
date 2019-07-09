6cefc2dcf967c1d8f668f0e6fef7c5687ed393a1
frmunoz <munozfra@lthe.local>
2019-07-09 17:33:49 +0200

    Update tests of forward

--- pkg/tests/testthat/test-forward.R
+++ pkg/tests/testthat/test-forward.R
@@ -1,32 +1,34 @@
 context("Test forward() structure and extreme cases")
 
 test_that("forward() works and returns desired result structure", {
-  pool <- data.frame(id = 1:10000,
+  pool <- data.frame(ind = paste("pool",1:10000, sep="."),
                      sp = rep(1:500, each = 20),
                      trait = rep(runif(500), 20))
   
   set.seed(1)
-  initial <- pool[sample(nrow(pool), size = 50, replace = FALSE),]
+  initial <- data.frame(ind = paste("init",1:100, sep="."),
+                        sp = rep(1:10, each = 10),
+                        trait = rep(runif(10), 10))
   
   sink(tempfile())
-  res <- forward(initial, prob = 0.1, gens = 10, pool = pool, limit.sim = TRUE)
+  res <- forward(initial, prob = 0.1, gens = 5, pool = pool, limit.sim = TRUE)
   sink()
   
   expect_is(res, "list")
   expect_named(res, c("com", "sp_t", "dist.t", "pool", "call"))
   expect_is(res$com, "data.frame")
   expect_is(res$pool, "data.frame")
-  expect_named(res$com, c("id", "sp", "trait"))
-  expect_named(res$pool, c("id", "sp", "trait"))
+  expect_named(res$com, c("ind", "sp", "trait"))
+  expect_named(res$pool, c("ind", "sp", "trait"))
   expect_is(res$com$trait, "numeric")
   expect_is(res$pool$trait, "numeric")
-  expect_equal(dim(res$com), c(50, 3))
+  expect_equal(dim(res$com), c(100, 3))
   expect_equal(dim(res$pool), dim(pool))
   
   
   # Simulations when keeping all generations
   sink(tempfile())
-  res_keep <- forward(initial[, -1], prob = 0.1, gens = 4, pool = pool,
+  res_keep <- forward(initial, prob = 0.1, gens = 5, pool = pool,
                       limit.sim = FALSE, keep = TRUE)
   sink()
   
@@ -35,17 +37,17 @@ test_that("forward() works and returns desired result structure", {
   expect_is(res_keep$com_t, "list")
   expect_is(res_keep$com_t[[1]], "data.frame")
   expect_is(res_keep$pool, "data.frame")
-  expect_named(res_keep$com_t[[1]], c("id", "sp", "trait"))
-  expect_named(res_keep$pool, c("id", "sp", "trait"))
+  expect_named(res_keep$com_t[[1]], c("ind", "sp", "trait"))
+  expect_named(res_keep$pool, c("ind", "sp", "trait"))
   expect_is(res_keep$com_t[[1]]$trait, "numeric")
   expect_is(res_keep$pool$trait, "numeric")
-  expect_equal(dim(res_keep$com_t[[1]]), c(50, 3))
-  expect_length(res_keep$com_t, 4)
+  expect_equal(dim(res_keep$com_t[[1]]), c(100, 3))
+  expect_length(res_keep$com_t, 5)
   expect_equal(dim(res_keep$pool), dim(pool))
   
   # With limiting similarity
   sink(tempfile())
-  res_limit <- forward(initial[, -1], prob = 0.1, gens = 4, pool = pool,
+  res_limit <- forward(initial, prob = 0.1, gens = 5, pool = pool,
                       limit.sim = TRUE, keep = TRUE)
   sink()
   
@@ -55,12 +57,12 @@ test_that("forward() works and returns desired result structure", {
   expect_is(res_limit$com_t[[1]], "data.frame")
   expect_is(res_limit$pool, "data.frame")
   expect_is(res_limit$dist.t, "numeric")
-  expect_named(res_limit$com_t[[1]], c("id", "sp", "trait"))
-  expect_named(res_limit$pool, c("id", "sp", "trait"))
+  expect_named(res_limit$com_t[[1]], c("ind", "sp", "trait"))
+  expect_named(res_limit$pool, c("ind", "sp", "trait"))
   expect_is(res_limit$com_t[[1]]$trait, "numeric")
   expect_is(res_limit$pool$trait, "numeric")
-  expect_equal(dim(res_limit$com_t[[1]]), c(50, 3))
-  expect_length(res_limit$com_t, 4)
+  expect_equal(dim(res_limit$com_t[[1]]), c(100, 3))
+  expect_length(res_limit$com_t, 5)
   expect_equal(dim(res_limit$pool), dim(pool))
-  expect_length(res_limit$dist.t, 4)
+  expect_length(res_limit$dist.t, 5)
 })
